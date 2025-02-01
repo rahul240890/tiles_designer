9f38e9bf8b229e9aef8e15cf568317f9a56c53d9
Rahul Keshwala <keshwala.rahul@gmail.com>
2025-02-01 17:20:32 +0530

    Initial project setup

---
+++ backend/test_db.py
@@ -0,0 +1,8 @@
+from database import engine
+
+try:
+    conn = engine.connect()
+    print("✅ PostgreSQL Connected Successfully!")
+    conn.close()
+except Exception as e:
+    print("❌ Connection Failed:", e)
